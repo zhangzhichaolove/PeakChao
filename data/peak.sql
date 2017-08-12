b7f7e55e7e6808c490b19733e834d1735e8925b8
zhangzhichao <zhangzhichaolove@vip.qq.com>
2017-08-12 22:43:32 +0800

    add data

---
+++ data/peak.sql
@@ -0,0 +1,49 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50527
+Source Host           : localhost:3306
+Source Database       : peak
+
+Target Server Type    : MYSQL
+Target Server Version : 50527
+File Encoding         : 65001
+
+Date: 2017-08-12 22:42:03
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'chao', 'chaochao');
+
+-- ----------------------------
+-- Table structure for `website`
+-- ----------------------------
+DROP TABLE IF EXISTS `website`;
+CREATE TABLE `website` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dayCount` int(10) NOT NULL DEFAULT '0',
+  `visitCount` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of website
+-- ----------------------------
+INSERT INTO `website` VALUES ('1', '0', '67');
