0a7df020f6d5bb249340018f47249a04038832f2
zhangzhichao <zhangzhichaolove@vip.qq.com>
2017-08-12 23:45:59 +0800

    update sql

--- data/peak.sql
+++ data/peak.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50527
 File Encoding         : 65001
 
-Date: 2017-08-12 22:42:03
+Date: 2017-08-12 23:45:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,14 +23,16 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(10) NOT NULL,
   `password` varchar(16) NOT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
+  `activation` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'chao', 'chaochao');
 
 -- ----------------------------
 -- Table structure for `website`
@@ -46,4 +48,4 @@ CREATE TABLE `website` (
 -- ----------------------------
 -- Records of website
 -- ----------------------------
-INSERT INTO `website` VALUES ('1', '0', '67');
+INSERT INTO `website` VALUES ('1', '0', '82');
